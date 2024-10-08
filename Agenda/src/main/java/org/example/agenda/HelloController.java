a8bcfbeb6bb44bb14ee05db833ca7f4510482d7f
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-10-08 10:13:53 +0200

    agenda comienzo

---
+++ Agenda/src/main/java/org/example/agenda/HelloController.java
@@ -0,0 +1,14 @@
+package main.java.org.example.agenda;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
