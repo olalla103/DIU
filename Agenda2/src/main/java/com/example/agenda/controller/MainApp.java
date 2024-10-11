a32bd2dfd9128ab43f5430fd3ef665c8b2d5f929
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-10-11 14:31:24 +0200

    agenda

---
+++ Agenda2/src/main/java/com/example/agenda/controller/MainApp.java
@@ -0,0 +1,164 @@
+package com.example.agenda.controller;
+
+import java.io.IOException;
+
+import com.example.agenda.model.Person;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import com.example.agenda.controller.Person_Overview_Controller;
+
+
+public class MainApp extends Application {
+
+    /*Vamos a crear una lista de objetos de tipo Person dentro de la clase principal MainApp.
+    El resto de controladores obtendrá luego acceso a esa lista central dentro de MainApp. */
+
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Olalla", "López"));
+        personData.add(new Person("Iñigo", "López"));
+        personData.add(new Person("Paola", "Bernal"));
+        personData.add(new Person("Berta", "Bernal"));
+        personData.add(new Person("Valentín", "López"));
+        personData.add(new Person("Rosa María", "Navarro-Casas"));
+        personData.add(new Person("Angelita", "Romero"));
+        personData.add(new Person("Antonio", "Navarro-Casas"));
+        personData.add(new Person("Carmen", "Meneses"));
+    }
+
+    /**
+     * Returns the data as an observable list of Persons.
+     *
+     * @return
+     */
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+//Hast aquí la creación de la lista
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+        // logo app
+        this.primaryStage.getIcons().add(new Image("icono.png"));
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("icono.png"));
+
+        initRootLayout();
+
+        showPersonOverview();
+
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/com/example/agenda/Root_Layout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/com/example/agenda/Person_Overview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            Person_Overview_Controller controller = loader.getController();
+            controller.setMainApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     *
+     * @return
+     */
+
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/com/example/agenda/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Agenda2/src/main/java/com/example/agenda/controller/Person_Overview_Controller.java
@@ -0,0 +1,155 @@
+package com.example.agenda.controller;
+
+import com.example.agenda.util.DateUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import com.example.agenda.model.Person;
+
+public class Person_Overview_Controller {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstName;
+    @FXML
+    private Label lastName;
+    @FXML
+    private Label street;
+    @FXML
+    private Label postalCode;
+    @FXML
+    private Label city;
+    @FXML
+    private Label birthday;
+
+    // Reference to the main application.
+    private MainApp mainApp;
+
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public Person_Overview_Controller() {
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+        // Initialize the person table with the two columns.
+        firstNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().lastNameProperty());
+
+        // Clear person details.
+        showPersonDetails(null);
+
+        // Listen for selection changes and show the person details when changed.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+
+
+    /**
+     * Fills all text fields to show details about the person.
+     * If the specified person is null, all text fields are cleared.
+     *
+     * @param person the person or null
+     */
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            // Fill the labels with info from the person object.
+            firstName.setText(person.getFirstName());
+            lastName.setText(person.getLastName());
+            street.setText(person.getStreet());
+            postalCode.setText(Integer.toString(person.getPostalCode()));
+            city.setText(person.getCity());
+
+            // TODO: We need a way to convert the birthday into a String!
+            birthday.setText(DateUtil.format(person.getBirthday()));
+        } else {
+            // Person is null, remove all the text.
+            firstName.setText("");
+            lastName.setText("");
+            street.setText("");
+            postalCode.setText("");
+            city.setText("");
+            birthday.setText("");
+        }
+    }
+
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+            alert.showAndWait();
+        }
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Add observable list data to the table
+        personTable.setItems(mainApp.getPersonData());
+    }
+
+    /**
+     * Called when the user clicks the new button. Opens a dialog to edit
+     * details for a new person.
+     */
+    @FXML
+    private void handleNewPerson() {
+        Person tempPerson = new Person();
+        boolean okClicked = mainApp.showPersonEditDialog(tempPerson);
+        if (okClicked) {
+            mainApp.getPersonData().add(tempPerson);
+        }
+    }
+
+    /**
+     * Called when the user clicks the edit button. Opens a dialog to edit
+     * details for the selected person.
+     */
+
+    @FXML
+    private void handleEditPerson() {
+        Person selectedPerson = personTable.getSelectionModel().getSelectedItem();
+        if (selectedPerson != null) {
+            boolean okClicked = mainApp.showPersonEditDialog(selectedPerson);
+            if (okClicked) {
+                showPersonDetails(selectedPerson);
+            }
+        } else {
+            // Nothing selected.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+            alert.showAndWait();
+        }
+
+    }
+}
+
