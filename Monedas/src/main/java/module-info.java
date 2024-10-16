ff43950cb14b2f5cb75fbb2b1ee0c018480b35c1
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-10-16 12:52:35 +0200

    conversor

---
+++ Monedas/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module org.example.monedas {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires AccesoBBDDMoneda;
+    requires java.sql;
+
+    opens org.example.monedas to javafx.fxml;
+    exports org.example.monedas;
+    exports org.example.monedas.controller;
+    opens org.example.monedas.controller to javafx.fxml;
+}
---
+++ Monedas/src/main/java/org/example/monedas/controller/HelloController.java
@@ -0,0 +1,10 @@
+package org.example.monedas.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+}
