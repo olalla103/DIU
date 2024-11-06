52635d957392413b28ecae2bbe607839803c4af4
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-11-06 12:54:18 +0100

    a

---
+++ GestionHotel/src/main/java/org/example/gestionhotel/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.gestionhotel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
