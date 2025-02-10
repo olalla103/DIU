74b90f1545a780b0f4969ebec2b3fa700bd8ae50
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2025-02-10 09:37:49 +0100

    modificada la agenda

--- AgendaReact/MicroServicioAgenda/src/main/java/com/example/MicroServicioAgenda/repository/PersonRepository.java
+++ AgendaReact/MicroServicioAgenda/src/main/java/com/example/MicroServicioAgenda/repository/PersonRepository.java
@@ -7,13 +7,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends MongoRepository<PersonDto, String> {
-    List<PersonDto> findByPublished();
 
     List<PersonDto> findAll();
 
     Optional<PersonDto> getPersonByDNI();
 
-    List<PersonDto> findByTitleContaining(String title);
 
-    List<PersonDto> findByPublished(Boolean published);
 }
